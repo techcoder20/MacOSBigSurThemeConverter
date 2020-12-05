1dcbd4b775fa05c62f5025c637c8b61e6510b89b
techcoder <techcoder20@gmail.com>
2020-12-05 05:32:43 -0600

    Updated Restore

--- RevertBackToDefaultConfigs.sh
+++ RevertBackToDefaultConfigs.sh
@@ -1,21 +1,35 @@
 #!/bin/bash
-RED='\033[0;31m'
-NC='\033[0m' 
 
-printf "${RED}THIS SCRIPT WILL REVERT BACK THE SYSTEM TO THE DEFAULT RASPBERRY PI OS THEME AND THE THEME WHEN YOU RAN THE CONVERTER SCRIPT${NC}\n"
-read -n 1 -s -r -p "IF THATS WHAT YOU WANT PRESS ANY BUTTON OF CLICK ctrl+z TO EXIT: "
-echo " "
-
-
+#Removing Existing Configs
 sudo rm -r /home/pi/.config/lxpanel
 sudo rm -r /home/pi/.config/lxterminal
 sudo rm -r /home/pi/.config/lxsession
+#restoring backups
+cp -r  ~/.MacOSBigSurThemeassets/.config_backup/lxpanel ~/.config
+cp -r  ~/.MacOSBigSurThemeassets/.config_backup/lxsession ~/.config
+cp -r  ~/.MacOSBigSurThemeassets/.config_backup/lxterminal ~/.config
 pcmanfm --set-wallpaper=/usr/share/rpd-wallpaper/temple.jpg
+#Removing autostart shortcuts
 sudo rm /home/pi/.config/autostart/compton.desktop
 sudo rm /home/pi/.config/autostart/plank.desktop
 sudo rm /home/pi/.config/autostart/xfwm4.desktop
+#Restoring Default Desktop File For Chromium
 sudo cp /home/pi/.MacOSBigSurThemeassets/assets/extra/chromium-browser.desktop /usr/share/applications/
-
+#Refreshing Desktop
+sudo update-icon-caches /usr/share/icons/*
+sleep 1
+openbox --restart &>/dev/null
+killall tint2 pcmanfm lxpanel &>/dev/null
+rm -r /home/pi/.cache/tint2 &>/dev/null
+sleep 5
+tint2 &>/dev/null &
+sleep 1
+lxpanel --profile LXDE-pi &>/dev/null &
+sleep 2
+openbox --restart &>/dev/null
+sleep 1
+pcmanfm --desktop --profile LXDE-pi &>/dev/null &
+sleep 2
 
 read -n 1 -s -r -p "YOUR SYSTEM WILL REBOOT NOW PRESS ANY KEY"
 sudo reboot now
