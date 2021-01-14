2d4f8d3384f0e7b519cb29959b126b115ad7eda2
RPICoder <68472305+techcoder20@users.noreply.github.com>
2021-01-14 10:40:31 +0530

    Update uninstall.sh

--- uninstall.sh
+++ uninstall.sh
@@ -7,18 +7,38 @@ sudo rm -r ~/.config/lxsession
 sudo rm -r ~/.config/pcmanfm
 
 #restoring backups
-cp -r  ~/.MacOSBigSurThemeassets/.config_backup/lxpanel ~/.config
-cp -r  ~/.MacOSBigSurThemeassets/.config_backup/lxsession ~/.config
-cp -r  ~/.MacOSBigSurThemeassets/.config_backup/lxterminal ~/.config
-cp -r  ~/.MacOSBigSurThemeassets/.config_backup/pcmanfm ~/.config
+cp -r  ~/.local/share/MacOSBigSurThemeConverter/.config_backup/lxpanel ~/.config
+cp -r  ~/.local/share/MacOSBigSurThemeConverter/.config_backup/lxsession ~/.config
+cp -r  ~/.local/share/MacOSBigSurThemeConverter/.config_backup/lxterminal ~/.config
+cp -r  ~/.local/share/MacOSBigSurThemeConverter/.config_backup/pcmanfm ~/.config
 
 #Removing autostart shortcuts
-sudo rm /home/pi/.config/autostart/compton.desktop
-sudo rm /home/pi/.config/autostart/plank.desktop
-sudo rm /home/pi/.config/autostart/xfwm4.desktop
+sudo rm /home/pi/.config/autostart/MacOSBigSurThemeConverterAutostart.desktop
+
+#Removing Gtk Themes
+rm -r ~/.themes/WhiteSur-dark/
+rm -r ~/.themes/WhiteSur-light/
+
+#Removing Icon Themes
+rm -r ~/.local/share/icons/BigSur/
+rm -r ~/.local/share/icons/BigSur-dark/
+
+#Removing Cursors
+sudo rm -r /usr/share/icons/BigSur-Originals-Cursor/
+
+#Removing Plank Theme
+rm -r ~/.local/share/plank/themes/mcOS-BS-Black-Stock/
+rm -r ~/.local/share/plank/themes/mcOS-BS-White-Stock/
+
+#Removing Ulauncher, xfce4-panel-profiles, lightpad
+sudo apt purge ulauncher xfce4-panel-profiles lightpad
+
+#Removing Network Manager
+sudo sed -i '/denyinterfaces wlan0/c\ ' /etc/dhcpcd.conf 
+sudo sed -i '/[main]/,/managed=true/d' /etc/NetworkManager/NetworkManager.conf 
 
 #Restoring Default Desktop File For Chromium
-sudo cp /home/pi/.MacOSBigSurThemeassets/assets/extra/chromium-browser.desktop /usr/share/applications/
+sudo sed -i '/Icon/c\Icon=chromium-browser' /usr/share/applications/chromium-browser.desktop
 
 echo "Finished Restoring Config Files.Refreshing desktop session now, but rebooting is recommended."
 
