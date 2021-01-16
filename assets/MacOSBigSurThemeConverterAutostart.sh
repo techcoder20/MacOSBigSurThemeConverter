0577303a6359e5f14c5394c0d4439580c618ec8e
RPICoder <68472305+techcoder20@users.noreply.github.com>
2021-01-16 11:41:00 +0530

    Added ulauncher to startup script

--- assets/MacOSBigSurThemeConverterAutostart.sh
+++ assets/MacOSBigSurThemeConverterAutostart.sh
@@ -5,6 +5,7 @@ nohup compton &>/dev/null &
 nohup xfwm4 --replace --compositor=off &>/dev/null &
 nohup plank &>/dev/null &
 nohup xfce4-panel &>/dev/null &
+env GDK_BACKEND=x11 /usr/bin/ulauncher --hide-window --hide-window
 
 if [ -f ~/.local/share/MacOSBigSurThemeConverter/assets/.MacOSBigSurLight ]; then
     pcmanfm --set-wallpaper="/home/pi/.local/share/MacOSBigSurThemeConverter/assets/MacOSBigSurLight/MacOSBigSurLightWallpaper.jpg" &>/dev/null &
